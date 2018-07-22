24657a2dcd191e2fd4d9a8fa918bd8eadcbf70f0
Martin Røed <martin.roed@cognite.com>
2018-07-22 14:41:00 +0200

    Cors.

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,4 +1,5 @@
 import * as Koa from 'koa';
+import * as cors from '@koa/cors';
 
 import { logger } from './logging';
 import { routes } from './routes';
@@ -11,7 +12,7 @@ async function responseTimeMiddleware(ctx: Koa.Context, next: () => {}) {
   const timeSpent = Date.now() - now;
   ctx.set('X-Response-Time', `${timeSpent}ms`);
 }
-
+app.use(cors());
 app.use(responseTimeMiddleware);
 app.use(logger);
 app.use(routes);
