5aa000648c7705483efa7ca306eece02850b6c8a
Martin Røed <msroed@gmail.com>
2018-07-23 03:25:33 +0200

    Added redis cache.

--- src/server/config.ts
+++ src/server/config.ts
@@ -1,11 +1,15 @@
 export interface IConfig {
   port: number;
   prettyLog: boolean;
+  redisHost?: string;
+  redisPort?: number;
 }
 
 const config: IConfig = {
   port: +process.env.port || 3000,
   prettyLog: process.env.NODE_ENV !== 'production',
+  redisHost: process.env.REDIS_HOST,
+  redisPort: +process.env.REDIS_PORT,
 };
 
 export { config };
--- src/server/server.ts
+++ src/server/server.ts
@@ -3,6 +3,7 @@ import * as cors from '@koa/cors';
 
 import { logger } from './logging';
 import { routes } from './routes';
+import * as cache from 'koa-redis-cache';
 
 const app = new Koa();
 
@@ -12,9 +13,16 @@ async function responseTimeMiddleware(ctx: Koa.Context, next: () => {}) {
   const timeSpent = Date.now() - now;
   ctx.set('X-Response-Time', `${timeSpent}ms`);
 }
+
 app.use(cors());
 app.use(responseTimeMiddleware);
 app.use(logger);
+app.use(
+  cache({
+    routes: ['/show/:id', '/get:id', '/search(.*)'],
+    expire: 60 * 60 * 24, // one day
+  })
+);
 app.use(routes);
 
 export default app;
