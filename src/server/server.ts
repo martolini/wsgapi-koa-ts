76fb74961ab1b0b59ff89ce79c999a84c462005a
Martin Røed <martin.roed@cognite.com>
2018-09-01 00:16:56 +0200

    Removed cache>

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,10 +1,10 @@
-import * as Koa from 'koa';
-import * as cors from '@koa/cors';
+import * as Koa from "koa";
+import * as cors from "@koa/cors";
 
-import { logger } from './logging';
-import { config } from './config';
-import { routes } from './routes';
-import * as cache from 'koa-redis-cache';
+import { logger } from "./logging";
+import { config } from "./config";
+import { routes } from "./routes";
+import * as cache from "koa-redis-cache";
 
 const app = new Koa();
 
@@ -12,21 +12,12 @@ async function responseTimeMiddleware(ctx: Koa.Context, next: () => {}) {
   const now = Date.now();
   await next();
   const timeSpent = Date.now() - now;
-  ctx.set('X-Response-Time', `${timeSpent}ms`);
+  ctx.set("X-Response-Time", `${timeSpent}ms`);
 }
 
 app.use(cors());
 app.use(responseTimeMiddleware);
 app.use(logger);
-app.use(
-  cache({
-    routes: ['/show/:id', '/get:id', '/search(.*)'],
-    expire: 60 * 60 * 24, // one day
-    redis: {
-      host: config.redisHost,
-    },
-  })
-);
 app.use(routes);
 
 export default app;
